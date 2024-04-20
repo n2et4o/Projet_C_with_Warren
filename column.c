8d43ebed8381b27052fbebe21fa721a3b9395256
n2et4o <117654110+n2et4o@users.noreply.github.com>
2024-04-20 13:05:41 +0200

    Update column.c
    uptades de NULL pour toutes les fonctions

--- column.c
+++ column.c
@@ -150,58 +150,262 @@ void print_col(COLUMN* col) {
 }
 
 // ========= Création de la fonction qui retourner le nombre de d’occurrences d’une valeur x (x donné en paramètre) ======
-int nb_occ(COLUMN* col, int x) {
+void nb_occ(COLUMN* col, void* x) {
     int count = 0;
+    ENUM_TYPE type = col->column_type;
+
     for (unsigned int i = 0; i < col->size; i++) {
-        if (col->data[i]->int_value == x) {
-            count++;
+        if (x == NULL) {
+            if (col->data[i] == NULL) {
+                count++;
+            }
+        } else if (col->data[i] != NULL) {
+            switch (type) {
+                case INT:
+                    if (*(int*)(col->data[i]) == *(int*)x) count++;
+                    break;
+                case FLOAT:
+                    if (*(float*)(col->data[i]) == *(float*)x) count++;
+                    break;
+                case CHAR:
+                    if (*(char*)(col->data[i]) == *(char*)x) count++;
+                    break;
+                case DOUBLE:
+                    if (*(double*)(col->data[i]) == *(double*)x) count++;
+                    break;
+                case STRING:
+                    if (strcmp((char*)(col->data[i]), (char*)x) == 0) count++;
+                    break;
+                case UINT:
+                    if (*(unsigned int*)(col->data[i]) == *(unsigned int*)x) count++;
+                    break;
+                default:
+                    // Potentially add cases for STRUCTURE or other custom types
+                    break;
+            }
         }
     }
-    return count;
+
+    if (x == NULL) {
+        printf("Le nombre d'occurrences de la valeur NULL est : %d\n", count);
+    } else {
+        // Assuming x is a pointer to a type that matches the column type
+        switch (type) {
+            case INT:
+            case UINT:
+                printf("Le nombre d'occurrences de la valeur %d est : %d\n", *(int*)x, count);
+                break;
+            case FLOAT:
+                printf("Le nombre d'occurrences de la valeur %f est : %d\n", *(float*)x, count);
+                break;
+            case CHAR:
+                printf("Le nombre d'occurrences de la valeur '%c' est : %d\n", *(char*)x, count);
+                break;
+            case DOUBLE:
+                printf("Le nombre d'occurrences de la valeur %lf est : %d\n", *(double*)x, count);
+                break;
+            case STRING:
+                printf("Le nombre d'occurrences de la valeur \"%s\" est : %d\n", (char*)x, count);
+                break;
+            default:
+                printf("Type non supporte pour l'affichage\n");
+                break;
+        }
+    }
 }
+
 
 // ========= Création de la fonction qui retourner la valeur présente à la position x (x donné en paramètre) ======
-int pos_val(COLUMN* col, unsigned int x) {
-    if (x >= col->size) {
+void pos_val(COLUMN* col, unsigned int x) {
+    if (col == NULL || x >= col->size) {
         printf("Position invalide\n");
-        exit(EXIT_FAILURE);
+        exit(EXIT_FAILURE); // Or handle the error as appropriate
     }
-    if (col->data[x] != NULL) {
-        return *((int*)col->data[x]);
+
+    if (col->data[x] == NULL) {
+        printf("La valeur a la position %u est : NULL\n", x);
+        return;
+    }
+
+    // Determine and print the value based on the type stored in the column
+    switch (col->column_type) {
+        case INT:
+            printf("La valeur a la position %u est : %d\n", x, *((int*)col->data[x]));
+            break;
+        case FLOAT:
+            printf("La valeur a la position %u est : %f\n", x, *((float*)col->data[x]));
+            break;
+        case CHAR:
+            printf("La valeur a la position %u est : '%c'\n", x, *((char*)col->data[x]));
+            break;
+        case DOUBLE:
+            printf("La valeur a la position %u est : %lf\n", x, *((double*)col->data[x]));
+            break;
+        case STRING:
+            printf("La valeur a la position %u est : \"%s\"\n", x, *((char**)col->data[x]));
+            break;
+        case UINT:
+            printf("La valeur a la position %u est : %u\n", x, *((unsigned int*)col->data[x]));
+            break;
+        case LONG:
+            printf("La valeur a la position %u est : %ld\n", x, *((long*)col->data[x]));
+            break;
+        case STRUCTURE:
+            // Assuming you have a way to stringify your structure
+            printf("La valeur a la position %u est : Structure\n", x);
+            break;
+        default:
+            printf("Type non supporte\n");
+            break;
+    }
+}
+
+
+void nb_val_supe(COLUMN* col, void* x) {
+    if (col == NULL) {
+        printf("Invalid column reference\n");
+        return;
+    }
+
+    int count = 0;
+    ENUM_TYPE type = col->column_type;
+
+    if (x == NULL) {
+        // Counting all non-NULL entries if x is NULL
+        for (unsigned int i = 0; i < col->size; i++) {
+            if (col->data[i] != NULL) {
+                count++;
+            }
+        }
+        printf("Le nombre de valeurs non-NULL est : %d\n", count);
     } else {
-        return -1;  // Retourner -1 ou autre valeur indicative si la position est NULL
+        // Perform type-appropriate comparisons
+        for (unsigned int i = 0; i < col->size; i++) {
+            if (col->data[i] != NULL) {
+                switch (type) {
+                    case INT:
+                        if (*((int*)(col->data[i])) > *((int*)x)) count++;
+                        break;
+                    case FLOAT:
+                        if (*((float*)(col->data[i])) > *((float*)x)) count++;
+                        break;
+                    case DOUBLE:
+                        if (*((double*)(col->data[i])) > *((double*)x)) count++;
+                        break;
+                    case UINT:
+                        if (*((unsigned int*)(col->data[i])) > *((unsigned int*)x)) count++;
+                        break;
+                    // Extend cases for other types if necessary
+                    default:
+                        printf("Type non supporté pour la comparaison\n");
+                        return;
+                }
+            }
+        }
+        // Print the results based on the assumed type of `x` being int
+        if (type == INT || type == UINT) {
+            printf("Le nombre de valeurs superieures a %d est : %d\n", *((int*)x), count);
+        } else if (type == FLOAT) {
+            printf("Le nombre de valeurs superieures a %f est : %d\n", *((float*)x), count);
+        } else if (type == DOUBLE) {
+            printf("Le nombre de valeurs superieures a %lf est : %d\n", *((double*)x), count);
+        }
     }
 }
 
-int nb_val_supe(COLUMN* col, int x) {
+
+void nb_val_inf(COLUMN* col, void* x) {
+    if (col == NULL || x == NULL) {
+        printf("Invalid input\n");
+        return;
+    }
+
     int count = 0;
+    ENUM_TYPE type = col->column_type;
+
     for (unsigned int i = 0; i < col->size; i++) {
-        if (col->data[i]->int_value > x) {
-            count++;
+        if (col->data[i] != NULL) {
+            switch (type) {
+                case INT:
+                    if (*((int*)(col->data[i])) < *((int*)x)) count++;
+                    break;
+                case FLOAT:
+                    if (*((float*)(col->data[i])) < *((float*)x)) count++;
+                    break;
+                case DOUBLE:
+                    if (*((double*)(col->data[i])) < *((double*)x)) count++;
+                    break;
+                default:
+                    printf("Comparaison inferieure non supportee pour ce type\n");
+                    return;
+            }
         }
     }
-    return count;
+
+    // Print the final count outside the loop
+    switch (type) {
+        case INT:
+            printf("Le nombre de valeurs inferieures a %d est : %d\n", *((int*)x), count);
+            break;
+        case FLOAT:
+            printf("Le nombre de valeurs inferieures a %f est : %d\n", *((float*)x), count);
+            break;
+        case DOUBLE:
+            printf("Le nombre de valeurs inferieures a %lf est : %d\n", *((double*)x), count);
+            break;
+        default:
+            printf("Type non supporte pour l'affichage\n");
+            break;
+    }
 }
 
-int nb_val_inf(COLUMN* col, int x) {
+
+void nb_val_egal(COLUMN* col, void* x) {
+    if (col == NULL || x == NULL) {
+        printf("Invalid input or comparison to NULL is not supported for non-pointer types\n");
+        return;
+    }
+
     int count = 0;
+    ENUM_TYPE type = col->column_type;
+
     for (unsigned int i = 0; i < col->size; i++) {
-        if (col->data[i]->int_value < x) {
-            count++;
+        if (col->data[i] != NULL) {
+            switch (type) {
+                case INT:
+                    if (*((int*)(col->data[i])) == *((int*)x)) count++;
+                    break;
+                case FLOAT:
+                    if (*((float*)(col->data[i])) == *((float*)x)) count++;
+                    break;
+                case DOUBLE:
+                    if (*((double*)(col->data[i])) == *((double*)x)) count++;
+                    break;
+                case UINT:
+                    if (*((unsigned int*)(col->data[i])) == *((unsigned int*)x)) count++;
+                    break;
+                case CHAR:
+                    if (*((char*)(col->data[i])) == *((char*)x)) count++;
+                    break;
+                default:
+                    printf("Comparison not supported for this type\n");
+                    return;
+            }
         }
     }
-    return count;
+
+    // Assuming x is of type int for the printf format specifier; adjust as necessary for other types
+    if (type == INT || type == UINT) {
+        printf("Le nombre de valeurs egales a %d est : %d\n", *((int*)x), count);
+    } else if (type == FLOAT) {
+        printf("Le nombre de valeurs egales a %f est : %d\n", *((float*)x), count);
+    } else if (type == DOUBLE) {
+        printf("Le nombre de valeurs egales a %lf est : %d\n", *((double*)x), count);
+    } else if (type == CHAR) {
+        printf("Le nombre de valeurs egales a '%c' est : %d\n", *((char*)x), count);
+    }
 }
 
-int nb_val_egal(COLUMN* col, int x) {
-    int count = 0;
-    for (unsigned int i = 0; i < col->size; i++) {
-        if (col->data[i]->int_value == x) {
-            count++;
-        }
-    }
-    return count;
-}
 
 void convert_value(COLUMN *col, unsigned long long int i, char *str, int size) {
     if (col == NULL || str == NULL || i >= col->size || size <= 0) {
@@ -236,5 +440,93 @@ void convert_value(COLUMN *col, unsigned long long int i, char *str, int size) {
         default:
             snprintf(str, size, "%s", "UNKNOWN TYPE");
             break;
+    }
+}
+
+void sort(COLUMN* col, int sort_dir) {
+    if (col->valid_index == 0) {
+        // If index is not valid (column not sorted), use quicksort
+        quicksort(col->index, col->data, 0, col->size - 1, sort_dir);
+    } else if (col->valid_index == -1) {
+        // If index is partially valid, use insertion sort
+        insertion_sort(col->index, col->data, col->size, sort_dir);
+    }
+
+    col->sort_dir = sort_dir;
+}
+
+// Quicksort implementation
+void quicksort(unsigned long long *index, COL_TYPE **data, int left, int right, int sort_dir) {
+    if (left < right) {
+        int pivot_index = left;
+        unsigned long long pivot = index[right];
+
+        for (int i = left; i < right; i++) {
+            if ((sort_dir == ASC && index[i] < pivot) || (sort_dir == DESC && index[i] > pivot)) {
+                unsigned long long temp_index = index[i];
+                index[i] = index[pivot_index];
+                index[pivot_index] = temp_index;
+
+                COL_TYPE *temp_data = data[i];
+                data[i] = data[pivot_index];
+                data[pivot_index] = temp_data;
+
+                pivot_index++;
+            }
+        }
+
+        unsigned long long temp_index = index[right];
+        index[right] = index[pivot_index];
+        index[pivot_index] = temp_index;
+
+        COL_TYPE *temp_data = data[right];
+        data[right] = data[pivot_index];
+        data[pivot_index] = temp_data;
+
+        quicksort(index, data, left, pivot_index - 1, sort_dir);
+        quicksort(index, data, pivot_index + 1, right, sort_dir);
+    }
+}
+
+// Insertion sort implementation
+void insertion_sort(unsigned long long *index, COL_TYPE **data, int size, int sort_dir) {
+    int i, j;
+    unsigned long long key;
+    COL_TYPE *data_key;
+
+    for (i = 1; i < size; i++) {
+        key = index[i];
+        data_key = data[i];
+        j = i - 1;
+
+        while (j >= 0 && ((sort_dir == ASC && index[j] > key) || (sort_dir == DESC && index[j] < key))) {
+            index[j + 1] = index[j];
+            data[j + 1] = data[j];
+            j = j - 1;
+        }
+
+        index[j + 1] = key;
+        data[j + 1] = data_key;
+    }
+}
+
+void print_col_by_index(COLUMN *col) {
+    if (col->valid_index != 1) {
+        // L'index n'est pas valide, affichez un message d'erreur
+        printf("Error: Index is not valid.\n");
+        return;
+    }
+
+    // Afficher les valeurs selon l'ordre séquentiel de l'index
+    if (col->sort_dir == ASC) {
+        for (unsigned long long i = 0; i < col->size; i++) {
+            unsigned long long index = col->index[i];
+            printf("[%llu] %s\n", index, col->data[index]->string_value);
+        }
+    } else {
+        for (long long i = col->size - 1; i >= 0; i--) {
+            unsigned long long index = col->index[i];
+            printf("[%llu] %s\n", index, col->data[index]->string_value);
+        }
     }
 }
